fb3e4734e5f8d4ecd9990a959fee7751049b5c42
Alexander Kudlai <alexander@kudlai.com>
2020-06-14 22:13:44 +0300

    • Tree view learning WIP;

---
+++ LearnKendoUiAspMvc/Controllers/TreeViewController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Kendo.Mvc.UI;
+using LearnKendoUiAspMvc.Models;
+
+namespace LearnKendoUiAspMvc.Controllers
+{
+    public class TreeViewController : Controller
+    {
+        private readonly NorthwindDbContext _dbContext;
+
+        public TreeViewController()
+        {
+            _dbContext = new NorthwindDbContext();
+        }
+
+        // GET: TreeView
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TreeListRead([DataSourceRequest] DataSourceRequest request, int? parentId)
+        {
+            var result = (_dbContext.GetAllRemote().ToTreeDataSourceResult(request,
+                e => e.EmployeeId,
+                e => e.ReportsTo,
+                e => id.HasValue ? e.ReportsTo == parentId : e.ReportsTo == null,
+                e => e
+            );
+            
+        }
+
+        public JsonResult Index([DataSourceRequest] DataSourceRequest request, int? id)
+        {
+            var result = ((EmployeeDirectoryService)employeeDirectory).GetAllRemote().ToTreeDataSourceResult(request,
+                e => e.EmployeeId,
+                e => e.ReportsTo,
+                e => id.HasValue ? e.ReportsTo == id : e.ReportsTo == null,
+                e => e
+            );
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult All([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = GetDirectory().ToTreeDataSourceResult(request,
+                e => e.EmployeeId,
+                e => e.ReportsTo,
+                e => e
+            );
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Destroy([DataSourceRequest] DataSourceRequest request, EmployeeDirectoryModel employee)
+        {
+            if (ModelState.IsValid)
+            {
+                employeeDirectory.Delete(employee, ModelState);
+            }
+
+            return Json(new[] { employee }.ToTreeDataSourceResult(request, ModelState));
+        }
+
+        public JsonResult Create([DataSourceRequest] DataSourceRequest request, EmployeeDirectoryModel employee)
+        {
+            if (ModelState.IsValid)
+            {
+                employeeDirectory.Insert(employee, ModelState);
+            }
+
+            return Json(new[] { employee }.ToTreeDataSourceResult(request, ModelState));
+        }
+
+        public JsonResult Update([DataSourceRequest] DataSourceRequest request, EmployeeDirectoryModel employee)
+        {
+            if (ModelState.IsValid)
+            {
+                employeeDirectory.Update(employee, ModelState);
+            }
+
+            return Json(new[] { employee }.ToTreeDataSourceResult(request, ModelState));
+        }
+
+        private IEnumerable<EmployeeDirectoryModel> GetDirectory()
+        {
+            return _dbContext.Employees.;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _dbContext.Dispose();
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LearnKendoUiAspMvc/Models/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace LearnKendoUiAspMvc.Models.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        // The Id.
+        public int EmployeeId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        // A nullable ParentId.
+        public int? ReportsTo { get; set; }
+
+        public string Address { get; set; }
+
+        // This is a case-sensitive property. Define it only if you want to use lazy-loading.
+        // If it is not defined, the TreeList will calculate and assign its value on the client.
+        public bool HasChildren { get; set; }
+    }
+}
